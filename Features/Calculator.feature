dca148d3906740ac4ccd07d11c23eed9ce2bf33c
Oleg Gresik <grek2409@gmail.com>
2022-05-05 14:17:43 +0500

    First commit.

---
+++ Features/Calculator.feature
@@ -0,0 +1,13 @@
+﻿Feature: Calculator
+
+ Scenario Outline: Opreations
+
+    When I ennter "<num1>" use "<operation>" and enter "<num2>"
+    Then I expect number "<result>"
+
+    Examples:
+    |num1|operation|num2| result|
+    |2   |+        |2   |4      |
+    |6   |-        |4   |2      |
+    |2   |*        |4   |8      |
+    |18  |/        |2   |9      |
---
+++ StepDefinitions/CalculatorStepDefinitions.cs
@@ -0,0 +1,42 @@
+namespace SpecFlowProjectSharedata.StepDefinitions
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        private readonly Calculator _calculator;
+        private int _result;
+
+        public CalculatorStepDefinitions(Calculator calculator)
+        {
+            _calculator = calculator;
+        }
+
+        [When("I ennter \"(.*?)\" use \"(.*?)\" and enter \"(.*?)\"")]
+        public void WhenTheSecondNumberIs(int numberFirst, string operation, int numberSecond)
+        {
+            _calculator.FirstNumber = numberFirst;
+            _calculator.SecondNumber = numberSecond;
+            switch (operation)
+            {
+                case "+":
+                    _result = _calculator.Add();
+                    break;
+                case "-":
+                    _result = _calculator.Subtract();
+                    break;
+                case "*":
+                    _result = _calculator.Multiply();
+                    break;
+                case "/":
+                    _result = _calculator.Divide();
+                    break;
+            }
+        }
+
+        [Then("I expect number \"(.*?)\"")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            _result.Should().Be(result);
+        }
+    }
+}
